3aba3b15dc1b1dd8379dc16622844a6618444ab2
Cara Cottingham <boops@Caras-MacBook-Pro.local>
2026-09-04 15:20:57 -0700

    Travis

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
+import React, { useEffect, useState } from "react";
+
+
+
+  // your existing JSX stays down here
 
 const HotCollections = () => {
+
+
+const [hotCollections, setHotCollections] = useState([]);
+
+async function fetchHotCollections() {
+  const response = await fetch(
+    "https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections"
+  );
+  const data = await response.json();
+console.log(data);
+setHotCollections(data);
+
+  
+}
+
+useEffect(() => {
+  fetchHotCollections();
+}, []);
+
+
+
   return (
+    
     <section id="section-collections" className="no-bottom">
       <div className="container">
         <div className="row">
@@ -14,25 +41,25 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
+          {hotCollections.map((collection, index) => (
             <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
               <div className="nft_coll">
                 <div className="nft_wrap">
                   <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
+                    <img src={collection.nftImage} />
                   </Link>
                 </div>
                 <div className="nft_coll_pp">
                   <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
+                    <img className="lazy pp-coll" src={collection.AuthorImage} alt="" />
                   </Link>
                   <i className="fa fa-check"></i>
                 </div>
                 <div className="nft_coll_info">
                   <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
+                    <h4>{collection.title}</h4>
                   </Link>
-                  <span>ERC-192</span>
+                  <span>{collection.code}</span>
                 </div>
               </div>
             </div>
