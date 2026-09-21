9114ddfa4766b50e80e8d73755ceb854cc77b454
Cara Cottingham <boops@Caras-MacBook-Pro.local>
2026-09-20 19:54:07 -0700

    TRavis

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 
